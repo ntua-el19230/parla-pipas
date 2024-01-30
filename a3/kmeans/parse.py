29995b5a2282c590c1471ff59a3b1a9c243aa4bb
Altan Avtzi <avtzialtan@hotmail.com>
2024-01-30 12:45:48 +0200

    fixed impl in benchmarks

--- a3/kmeans/parse.py
+++ a3/kmeans/parse.py
@@ -6,7 +6,7 @@ def parse_data(input_data):
     # Regular expression patterns
     dataset_pattern = r"dataset_size = (\d+.\d+) MB\s+numObjs = (\d+)\s+numCoords = (\d+)\s+numClusters = (\d+), block_size = (\d+)"
     seq_kmeans_pattern = r"Sequential Kmeans.*?total = ([\d.]+) ms"
-    gpu_kmeans_pattern = r"(\w+ GPU Kmeans).*?t_alloc: ([\d.]+) ms.*?t_alloc_gpu: ([\d.]+) ms.*?t_get_gpu: ([\d.]+) ms.*?nloops = (\d+)\s+: total = ([\d.]+) ms.*?t_loop_avg = ([\d.]+) ms.*?t_loop_min = ([\d.]+) ms.*?t_loop_max = ([\d.]+) ms"
+    gpu_kmeans_pattern = r"(\w+) GPU Kmeans.*?t_alloc: ([\d.]+) ms.*?t_alloc_gpu: ([\d.]+) ms.*?t_get_gpu: ([\d.]+) ms.*?nloops = (\d+)\s+: total = ([\d.]+) ms.*?t_loop_avg = ([\d.]+) ms.*?t_loop_min = ([\d.]+) ms.*?t_loop_max = ([\d.]+) ms"
 
     results = []
 
@@ -34,7 +34,7 @@ def parse_data(input_data):
 
             # Creating a dictionary for each dataset
             result = {
-                "implementation": gpu_impl,
+                "implementation": gpu_impl.lower(),
                 "size": size,
                 "coords": num_coords,
                 "clusters": num_clusters,
